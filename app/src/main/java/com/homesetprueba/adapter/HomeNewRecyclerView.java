27c21e1fff200f05f539b7d4c60b3862a865bffc
albertsanchez <albert.sanchez@playtown.com.ar>
2017-08-09 17:49:30 -0300

    ffggg

---
+++ app/src/main/java/com/homesetprueba/adapter/HomeNewRecyclerView.java
@@ -0,0 +1,16 @@
+package com.homesetprueba.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by albertsanchez on 9/8/17.
+ */
+
+public class HomeNewRecyclerView extends RecyclerView {
+
+    public HomeNewRecyclerView(Context context) {
+        super(context);
+    }
+
+}
